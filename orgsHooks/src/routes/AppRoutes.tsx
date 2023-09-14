76f3993bc4d9253ce6e7b234ae3618c147e4574a
betocostadev <robertomarx@gmail.com>
2023-09-14 09:36:02 -0300

    improved app routes

--- orgsHooks/src/routes/AppRoutes.tsx
+++ orgsHooks/src/routes/AppRoutes.tsx
@@ -4,15 +4,24 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import ProducerRoutes from './ProducerRoutes'
 import BestProducersRoutes from './BestProducersRoutes'
 
+import HeartIcon from '../assets/heart.svg'
+import HomeIcon from '../assets/home.svg'
+
 const AppRoutes = () => {
   const Tab = createBottomTabNavigator()
 
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({route}) => ({
           headerShown: false,
-        }}>
+          tabBarActiveTintColor: '#2a9f85',
+          tabBarInactiveTintColor: '#c7c7c7',
+          tabBarIcon: ({color}) => {
+            const Icon = route.name === 'Best Producers' ? HeartIcon : HomeIcon
+            return <Icon color={color} />
+          },
+        })}>
         <Tab.Screen name="Home" component={ProducerRoutes} />
         <Tab.Screen name="Best Producers" component={BestProducersRoutes} />
       </Tab.Navigator>
