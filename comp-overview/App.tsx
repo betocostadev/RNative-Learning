c7fde42e63169704771ad5373e7ff070d158a539
betocostadev <robertomarx@gmail.com>
2024-06-28 16:26:53 -0300

    Added some components to comp-ov

--- comp-overview/App.tsx
+++ comp-overview/App.tsx
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, View } from 'react-native'
+import Logo from './assets/native.png'
 
 export default function App() {
+  const logData = (message: string, event: any) => {
+    Alert.alert(message)
+    console.log('Event:', event)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Components Overview</Text>
+      <Text style={styles.title}>Components Overview</Text>
+      <Image source={Logo} style={styles.logo} />
+      <View
+        onTouchStart={(event) => {
+          logData('On Touch Start', event)
+        }}
+        onTouchEnd={(event) => {
+          logData('On Touch End', event)
+        }}
+      >
+        <Text style={styles.touchableText}>Touchable View</Text>
+      </View>
+      <Text selectable={true} style={styles.mainText}>
+        <Text>A selectable text - </Text>
+        <Text>
+          It is possible to mark a text as selectable. Also, you can use a Text
+          component{' '}
+        </Text>
+        <Text>
+          inside another text component so they are treated as a single text.
+        </Text>
+        <Text>
+          It's possible to also use this text as a{' '}
+          <Text
+            style={styles.boldText}
+            onPress={() => console.log('PRESSED!')}
+            onLongPress={() => console.log('LONG PRESSED!')}
+          >
+            pressable
+          </Text>
+        </Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -16,5 +53,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  touchableText: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'lightblue',
+  },
+  mainText: {
+    fontSize: 16,
+    margin: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  logo: {
+    marginTop: 10,
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderRadius: 4,
   },
 })
