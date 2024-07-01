355a35b04ca28f1688226e2e9b363fe6d635ea9e
betocostadev <robertomarx@gmail.com>
2024-07-01 16:49:15 -0300

    Improved comp overview

--- comp-overview/App.tsx
+++ comp-overview/App.tsx
@@ -1,5 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import { Alert, Image, StyleSheet, Text, View } from 'react-native'
+import {
+  Alert,
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native'
+import { useState } from 'react'
 import Logo from './assets/native.png'
 
 export default function App() {
@@ -8,42 +19,70 @@ export default function App() {
     console.log('Event:', event)
   }
 
+  const logEmailField = () => {
+    Alert.alert('Button Pressed', `Email: ${email}`)
+  }
+
+  const [email, setEmail] = useState('' as string)
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Components Overview</Text>
-      <Image source={Logo} style={styles.logo} />
-      <View
-        onTouchStart={(event) => {
-          logData('On Touch Start', event)
-        }}
-        onTouchEnd={(event) => {
-          logData('On Touch End', event)
-        }}
-      >
-        <Text style={styles.touchableText}>Touchable View</Text>
-      </View>
-      <Text selectable={true} style={styles.mainText}>
-        <Text>A selectable text - </Text>
-        <Text>
-          It is possible to mark a text as selectable. Also, you can use a Text
-          component{' '}
-        </Text>
-        <Text>
-          inside another text component so they are treated as a single text.
-        </Text>
-        <Text>
-          It's possible to also use this text as a{' '}
-          <Text
-            style={styles.boldText}
-            onPress={() => console.log('PRESSED!')}
-            onLongPress={() => console.log('LONG PRESSED!')}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.title}>Components Overview</Text>
+          <Image source={Logo} style={styles.logo} />
+
+          <TextInput
+            style={styles.input}
+            onChange={(event) => setEmail(event.nativeEvent.text)}
+            keyboardType="email-address"
+            accessibilityLabel="Email Input"
+            placeholder="Enter your email"
+            returnKeyType="done"
+            value={email}
+          />
+
+          <Button
+            title="Send email"
+            onPress={() => logEmailField()}
+            disabled={email.length < 5}
+          />
+
+          <View
+            onTouchStart={(event) => {
+              logData('On Touch Start', event)
+            }}
+            onTouchEnd={(event) => {
+              logData('On Touch End', event)
+            }}
           >
-            pressable
+            <Text style={styles.touchableText}>Touchable View</Text>
+          </View>
+          <Text selectable={true} style={styles.mainText}>
+            <Text>A selectable text - </Text>
+            <Text>
+              It is possible to mark a text as selectable. Also, you can use a
+              Text component{' '}
+            </Text>
+            <Text>
+              inside another text component so they are treated as a single
+              text.
+            </Text>
+            <Text>
+              It's possible to also use this text as a{' '}
+              <Text
+                style={styles.boldText}
+                onPress={() => console.log('PRESSED!')}
+                onLongPress={() => console.log('LONG PRESSED!')}
+              >
+                pressable
+              </Text>
+            </Text>
           </Text>
-        </Text>
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -55,6 +94,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
+    margin: 10,
     fontSize: 20,
     fontWeight: 'bold',
   },
@@ -69,6 +109,13 @@ const styles = StyleSheet.create({
   },
   boldText: {
     fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    padding: 8,
+    width: '60%',
   },
   logo: {
     marginTop: 10,
