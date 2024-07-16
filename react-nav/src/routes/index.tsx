d0872bcd29326d6f883ea3a04b905f2c64e31697
betocostadev <robertomarx@gmail.com>
2024-07-16 15:17:47 -0300

    Added bottom tabs to react nav

--- react-nav/src/routes/index.tsx
+++ react-nav/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StackRoutes } from './stack.routes'
+import { BottomTabsRoutes } from './bottomTabs.routes'
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {/* <StackRoutes /> */}
+      <BottomTabsRoutes />
     </NavigationContainer>
   )
 }
