ddb282dfbbceb60b7e6bdf0902a35f9fa3b0900b
betocostadev <robertomarx@gmail.com>
2025-05-12 19:51:18 +0100

    feat: adds user store persistance

--- frontend-masters/react-native-intermediate-v2/plantly/store/userStore.ts
+++ frontend-masters/react-native-intermediate-v2/plantly/store/userStore.ts
@@ -1,18 +1,30 @@
+import { PLANTY_USER_STORAGE_KEY } from '@/utils/constants'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 type TUserStore = {
   hasFinishedOnboarding: boolean
   toggleHasOnboarded: () => void
 }
 
-export const useUserStore = create<TUserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHasOnboarded: () => {
-    set((state) => {
-      return {
-        ...state,
-        hasFinishedOnboarding: !state.hasFinishedOnboarding,
-      }
-    })
-  },
-}))
+export const useUserStore = create(
+  persist<TUserStore>(
+    (set) => ({
+      // export const useUserStore = create<TUserStore>((set) => ({
+      hasFinishedOnboarding: false,
+      toggleHasOnboarded: () => {
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          }
+        })
+      },
+    }),
+    {
+      name: PLANTY_USER_STORAGE_KEY,
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+)
---
+++ frontend-masters/react-native-intermediate-v2/plantly/utils/constants.ts
@@ -0,0 +1,1 @@
+export const PLANTY_USER_STORAGE_KEY = 'planty-user-store'
